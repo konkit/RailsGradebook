27f427e7052d5fd367b6e51173ac950aa5010753
Lukasz Tenerowicz <lukasz.tenerowicz@polcode.net>
2015-08-06 14:30:04 +0200

    Added user seeds, added cancancan

--- Gemfile
+++ Gemfile
@@ -32,12 +32,12 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-gem 'devise'
-
 gem 'haml-rails'
 gem 'erb2haml'
 
+gem 'devise'
 gem 'rolify'
+gem 'cancancan'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+student1 = User.new
+student1.email = 'one@student.com'
+student1.password = '321321321'
+student1.password_confirmation = '321321321'
+student1.save!
+
+student1.add_role :student
+
+teacher1 = User.new
+teacher1.email = 'one@teacher.com'
+teacher1.password = '321321321'
+teacher1.password_confirmation = '321321321'
+teacher1.save!
+
+teacher1.add_role :teacher
+
+principal1 = User.new
+principal1.email = 'one@principal.com'
+principal1.password = '321321321'
+principal1.password_confirmation = '321321321'
+principal1.save!
+
+principal1.add_role :teacher
