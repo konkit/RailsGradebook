7e4594c44ecc4c5f95828b411aec02ccda03b8b3
Lukasz Tenerowicz <lukasz.tenerowicz@polcode.net>
2015-08-07 09:12:06 +0200

    Added student and teacher resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  resources :subjects, :grades, :divisions
+  resources :subjects, :grades, :divisions, :teachers, :students
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
